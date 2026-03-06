8c555f362d2e1fce642d34b7a4bf407ad48963d5
Muhamad Akmal Rizqillah <akmalrizqillah26@gmail.com>
2026-03-06 13:10:18 +0700

    Menambahkan folder Pertemuan-4

---
+++ Pertemuan-4/No3/Strings.java
@@ -0,0 +1,28 @@
+package No3;
+
+import java.util.Scanner;
+
+public class Strings{
+    public static void main(String[] args){
+        String[] strList;
+        int size;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("How many strings do you want to sort? ");
+        size = scan.nextInt();
+        scan.nextLine();
+
+        strList = new String[size];
+
+        System.out.println("Enter the strings...");
+        for (int i = 0; i < size; i++)
+            strList[i] = scan.nextLine();
+
+        Sorting.insertionSort(strList);
+
+        System.out.println("\nStrings in sorted order...");
+        for (int i = 0; i < size; i++)
+            System.out.println(strList[i]);
+    }
+}
